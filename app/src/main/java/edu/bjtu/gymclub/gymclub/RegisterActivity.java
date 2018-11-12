cde9bd5b74a65aa6f8e8b2c60a9a306a28584901
ylz <abc123@huawei.com>
2018-11-12 10:55:40 +0800

    update

--- app/src/main/java/edu/bjtu/gymclub/gymclub/RegisterActivity.java
+++ app/src/main/java/edu/bjtu/gymclub/gymclub/RegisterActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -48,16 +52,15 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //电话号
-                EditText userPhoneNumberEdit = (EditText) findViewById(R.id.register_phone_num);
+                EditText userPhoneNumberEdit = (EditText) findViewById(R.id.et_phoneNumber);
                 //电子邮箱
-                EditText userEmailEdit = (EditText) findViewById(R.id.register_email);
+                EditText userEmailEdit = (EditText) findViewById(R.id.et_email);
                 //用户名
-                EditText userNameEdit = (EditText) findViewById(R.id.register_username);
+                EditText userNameEdit = (EditText) findViewById(R.id.et_username);
                 //密码
-                EditText userPasswordEdit = (EditText) findViewById(R.id.register_password);
+                TextInputEditText userPasswordEdit = (TextInputEditText) findViewById(R.id.tiet_password);
                 //密码确认
-                EditText userPasswordConfirmEdit = (EditText) findViewById(R.id.register_password_confirm);
-
+                TextInputEditText userPasswordConfirmEdit = (TextInputEditText) findViewById(R.id.tiet_password_confirm);
 
                 String userPhoneNumber = userPhoneNumberEdit.getText().toString();
                 String userEmail = userEmailEdit.getText().toString();
@@ -66,7 +69,15 @@ public class RegisterActivity extends AppCompatActivity {
                 String userPasswordConfirm = userPasswordConfirmEdit.getText().toString();
 
 
+
                 String url = "http://10.0.2.2:8080/register";
+
+                if(!checkEmail(userEmail)||!checkPhoneNumber(userPhoneNumber)||!checkUserName(userName)
+                        ||!checkPasswords(userPassword,userPasswordConfirm)){
+                    return;
+                }
+
+
                 registerNameWordToServer(url, userPhoneNumber, userEmail, userName, userPassword);
 
 
@@ -76,6 +87,85 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
+    //验证邮箱是否合法
+    private boolean checkEmail(String email){
+        TextInputLayout til_email=findViewById(R.id.til_email);
+        til_email.setErrorEnabled(true);
+        if(TextUtils.isEmpty(email)){
+            showerror(til_email,"Email can not be empty!");
+            return false;
+        }else if(!email.contains("@")){
+            showerror(til_email,"Email is illegal!");
+            return false;
+        }else{
+            til_email.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    //检查电话号
+    private boolean checkPhoneNumber(String phoneNumber){
+        TextInputLayout til_phoneNumber=findViewById(R.id.til_phoneNumber);
+        til_phoneNumber.setErrorEnabled(true);
+        if(TextUtils.isEmpty(phoneNumber)){
+            showerror(til_phoneNumber,"Phone number can not be empty!");
+            return false;
+        }else{
+            til_phoneNumber.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    //检查用户名
+    private boolean checkUserName(String name){
+        TextInputLayout til_name=findViewById(R.id.til_username);
+        til_name.setErrorEnabled(true);
+        if(TextUtils.isEmpty(name)){
+            showerror(til_name,"Username can not be empty!");
+            return false;
+        }else{
+            til_name.setErrorEnabled(false);
+            return true;
+        }
+    }
+    //检查密码
+    private boolean checkPasswords(String password,String password_con){
+
+        TextInputLayout til_password=findViewById(R.id.til_password);
+        TextInputLayout til_passwordCon=findViewById(R.id.til_password_confirm);
+
+        til_password.setErrorEnabled(true);
+        til_passwordCon.setErrorEnabled(true);
+       if(password.length()<6){
+            showerror(til_password,"Enter a password with at least 6 characters!");
+            return false;
+        } else if (!password.equals(password_con)){
+            showerror(til_passwordCon,"The two passwords do not match!");
+           til_password.setErrorEnabled(false);
+            return false;
+        }else{
+           til_password.setErrorEnabled(false);
+           til_passwordCon.setErrorEnabled(false);
+            return true;
+        }
+
+    }
+
+    //显示错误
+    /**
+     * 显示错误提示，并获取焦点
+     * @param textInputLayout
+     * @param error
+     */
+    private void showerror(TextInputLayout textInputLayout,String error){
+        textInputLayout.setError(error);
+        textInputLayout.getEditText().setFocusable(true);
+        textInputLayout.getEditText().setFocusableInTouchMode(true);
+        textInputLayout.getEditText().requestFocus();
+    }
+
+
+    //后端验证
     private void registerNameWordToServer(String url, String userPhoneNumber, String userEmail, String userName, String userPassword) {
         OkHttpClient client = new OkHttpClient();
         FormBody.Builder formBuilder = new FormBody.Builder();
